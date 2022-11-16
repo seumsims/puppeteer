943098e2669d33bd7be3d3df9c6432b4a009be7b
seumsims <seumworldinbangladesh@gmail.com>
2022-11-16 22:06:47 +0600

    master puppeteer scrap html shop active it zone completed with console log done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://awesomescreenshot.s3.amazonaws.com/image/2444784/34434303-fcd05ed9e7c9eb5e78dd44ebb29337cd.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20221116%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221116T060835Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=5e212b48a02d4f90c73f2d0821c1ba10ac67f9a24ab24fc449726cb36cf67693');
+    await page.goto('https://shop.activeitzone.com/');
 
     
 
@@ -15,9 +15,13 @@ async function run() {
         }
         );  */
 
-    await page.screenshot({path: 'nice-tag-cloud-mdbootstrap-adamjakubowski-694459.png'}, {fullPage: true});
+    // await page.screenshot({path: 'nice-tag-cloud-mdbootstrap-adamjakubowski-694459.png'}, {fullPage: true});
+
+    const html = await page.content();
+    console.log(html);
     
     await browser.close();
+
     }
 
 run();
